0a3fa8ffe98f1bdcdb7f7d24991aaf4132e56bfe
Harings Rob <haringsrob@gmail.com>
2016-02-24 08:15:53 +0100

    Boilerplate icecatfetchertest

---
+++ tests/IcecatFetcherTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace haringsrob\Icecat\Test;
+
+use haringsrob\Icecat\Controller\IcecatFetcher;
+
+/**
+ * @coversDefaultClass \haringsrob\Icecat\Icecat
+ */
+class IcecatFetcherTests extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Tests the config methods of the icecatFetcher.
+     *
+     * @covers ::setUsername
+     * @covers ::setPassword
+     */
+    public function testFetcherConfig()
+    {
+        
+    }
+
+}
