18acbf83f901897306d2f641a69f238766e4f102
Harings Rob <haringsrob@gmail.com>
2018-03-08 16:37:29 +0100

    Update Result.php

--- src/Model/Result.php
+++ src/Model/Result.php
@@ -175,7 +175,7 @@ class Result implements ResultInterface
         if ($this->productHasProductFeature()) {
             $productFeature = $this->getProductData()->ProductFeature;
             
-            // Make sure $productFeature is an array
+            // Make sure $productFeature is an array.
             if (!is_array($productFeature)) {
                 $productFeature = [$productFeature];
             }
